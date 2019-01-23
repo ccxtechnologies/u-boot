64dfa8e3c5c631c7cf28e665c63e090e4e7f1efb
Charles Eidsness <charles@ccxtechnologies.com>
2019-01-23 16:02:08 -0500

    updating for systemx config

--- include/configs/ccimx6ulsbc.h
+++ include/configs/ccimx6ulsbc.h
@@ -11,7 +11,7 @@
 
 #include "ccimx6ul_common.h"
 
-#define CONFIG_BOARD_DESCRIPTION	"SBC Pro"
+#define CONFIG_BOARD_DESCRIPTION	"CCX DataPHY"
 
 /* uncomment for PLUGIN mode support */
 /* #define CONFIG_USE_PLUGIN */
@@ -28,7 +28,6 @@
 
 #if defined(CONFIG_SYS_BOOT_NAND)
 #define CONFIG_SYS_USE_NAND
-#define CONFIG_ENV_IS_IN_NAND
 #define CONFIG_CMD_MTDPARTS
 #define CONFIG_MTD_DEVICE
 #define CONFIG_MTD_PARTITIONS
@@ -36,7 +35,6 @@
 #define CONFIG_SYS_STORAGE_MEDIA	"nand"
 #define CONFIG_CMD_BOOTSTREAM
 #else
-#define CONFIG_ENV_IS_IN_MMC
 #define CONFIG_CMD_UPDATE_MMC
 #define CONFIG_SYS_STORAGE_MEDIA	"mmc"
 #endif
@@ -60,29 +58,11 @@
 
 #define CONFIG_SYS_FSL_USDHC_NUM	1
 
-/* U-Boot Environment */
-#if defined(CONFIG_ENV_IS_IN_MMC)
-#define CONFIG_ENV_OFFSET		(8 * SZ_64K)
-#elif defined(CONFIG_ENV_IS_IN_SPI_FLASH)
-#define CONFIG_ENV_OFFSET		(768 * 1024)
-#define CONFIG_ENV_SPI_BUS		CONFIG_SF_DEFAULT_BUS
-#define CONFIG_ENV_SPI_CS		CONFIG_SF_DEFAULT_CS
-#define CONFIG_ENV_SPI_MODE		CONFIG_SF_DEFAULT_MODE
-#define CONFIG_ENV_SPI_MAX_HZ		CONFIG_SF_DEFAULT_SPEED
-#elif defined(CONFIG_ENV_IS_IN_NAND)
-#undef CONFIG_ENV_SIZE
-#define CONFIG_ENV_OFFSET		(3 * SZ_1M)
-#define CONFIG_ENV_SIZE			SZ_128K
-#endif
-#define CONFIG_SYS_REDUNDANT_ENVIRONMENT
-#define CONFIG_DYNAMIC_ENV_LOCATION
-#if (CONFIG_DDR_MB == 1024)
-# define CONFIG_ENV_PARTITION_SIZE	(3 * SZ_1M)
-#else
-# define CONFIG_ENV_PARTITION_SIZE	(1 * SZ_1M)
-#endif /* (CONFIG_DDR_MB == 1024) */
-/* The environment may use any good blocks within the "environment" partition */
-#define CONFIG_ENV_RANGE		CONFIG_ENV_PARTITION_SIZE
+/* U-Boot Environment on eMMC*/
+#define CONFIG_ENV_IS_IN_FAT
+#define FAT_ENV_INTERFACE "mmc"
+#define FAT_ENV_DEVICE_AND_PART "0:1"
+#define FAT_ENV_FILE "uboot-env.bin"
 
 /* Serial port */
 #define CONFIG_MXC_UART
@@ -91,144 +71,54 @@
 #define CONFIG_CONSOLE_PORT		"ttymxc4"
 #define CONFIG_BAUDRATE			115200
 
+#define CONFIG_NETCONSOLE    1
+
 #define CONFIG_MODULE_FUSE
 #define CONFIG_OF_SYSTEM_SETUP
 
 /* Ethernet */
-#define CONFIG_FEC_ENET_DEV		0
-#if (CONFIG_FEC_ENET_DEV == 0)
 #define IMX_FEC_BASE			ENET_BASE_ADDR
 #define CONFIG_FEC_MXC_PHYADDR          0x0
 #define CONFIG_FEC_XCV_TYPE             RMII
-#elif (CONFIG_FEC_ENET_DEV == 1)
-#define IMX_FEC_BASE			ENET2_BASE_ADDR
-#define CONFIG_FEC_MXC_PHYADDR          0x1
-#define CONFIG_FEC_XCV_TYPE             RMII
-#endif
-
-#define CONFIG_DEFAULT_FDT_FILE		"zImage-imx6ul-" CONFIG_SYS_BOARD ".dtb"
 
 #define CONFIG_SYS_MMC_IMG_LOAD_PART	1
 
-#ifdef CONFIG_SECURE_BOOT
-/*
- * Authenticate bootscript before running it. IVT offset is at
- * ${filesize} - CONFIG_CSF_SIZE - IVT_SIZE (0x20)
- * Use 0x4000 as CSF_SIZE, as this is the value used by the script
- * to sign / encrypt the bootscript
- */
-#define CONFIG_BOOTCOMMAND \
-	"if run loadscript; then " \
-		"setexpr bs_ivt_offset ${filesize} - 0x4020;" \
-		"if hab_auth_img ${loadaddr} ${bs_ivt_offset}; then " \
-			"source ${loadaddr};" \
-		"fi; " \
-	"fi;"
-#else
-#define CONFIG_BOOTCOMMAND \
-	"if run loadscript; then " \
-		"source ${loadaddr};" \
-	"fi;"
+/* NOTE: These env settings are just in case the environment
+ * in NAND is coruppted, otherwise it is used instead. */
 
-#endif	/* CONFIG_SECURE_BOOT */
-
-#define CONFIG_COMMON_ENV	\
-	CONFIG_DEFAULT_NETWORK_SETTINGS \
-	CONFIG_EXTRA_NETWORK_SETTINGS \
-	"boot_fdt=yes\0" \
-	"bootargs_nfs=" \
-		"if test ${ip_dyn} = yes; then " \
-			"bootargs_ip=\"ip=dhcp\";" \
-		"else " \
-			"bootargs_ip=\"ip=\\${ipaddr}:\\${serverip}:" \
-			"\\${gatewayip}:\\${netmask}:\\${hostname}:" \
-			"eth0:off\";" \
-		"fi;\0" \
-	"bootargs_nfs_linux=run bootargs_nfs;" \
-		"setenv bootargs console=${console},${baudrate} " \
-		"${bootargs_linux} root=/dev/nfs " \
-		"${bootargs_ip} nfsroot=${serverip}:${rootpath},v3,tcp " \
-		"${mtdparts} ${bootargs_once} ${extra_bootargs}\0" \
-	"bootargs_tftp=" \
-		"if test ${ip_dyn} = yes; then " \
-			"bootargs_ip=\"ip=dhcp\";" \
-		"else " \
-			"bootargs_ip=\"ip=\\${ipaddr}:\\${serverip}:" \
-			"\\${gatewayip}:\\${netmask}:\\${hostname}:" \
-			"eth0:off\";" \
-		"fi;\0" \
-	"bootargs_tftp_linux=run bootargs_tftp;" \
-		"setenv bootargs console=${console},${baudrate} " \
-		"${bootargs_linux} root=/dev/nfs " \
-		"${bootargs_ip} nfsroot=${serverip}:${rootpath},v3,tcp " \
-		"${mtdparts} ${bootargs_once} ${extra_bootargs}\0" \
-	"console=" CONFIG_CONSOLE_PORT "\0" \
-	"dboot_kernel_var=zimage\0" \
-	"fdt_addr=0x83000000\0" \
-	"fdt_file=" CONFIG_DEFAULT_FDT_FILE "\0" \
-	"fdt_high=0xffffffff\0"	  \
-	"initrd_addr=0x83800000\0" \
-	"initrd_file=uramdisk.img\0" \
-	"initrd_high=0xffffffff\0" \
-	"recovery_file=recovery.img\0" \
-	"script=boot.scr\0" \
-	"uboot_file=u-boot.imx\0" \
-	"zimage=zImage-" CONFIG_SYS_BOARD ".bin\0"
-
-#if defined(CONFIG_SYS_BOOT_NAND)
 #define CONFIG_EXTRA_ENV_SETTINGS \
-	CONFIG_COMMON_ENV \
-	CONFIG_ENV_MTD_SETTINGS \
-	"bootargs_linux=\0" \
-	"bootargs_nand_linux=setenv bootargs console=${console},${baudrate} " \
-		"${bootargs_linux} ${mtdparts} ubi.mtd=${mtdlinuxindex} " \
-		"ubi.mtd=${mtdrootfsindex} root=ubi1_0 " \
-		"rootfstype=ubifs rw " \
-		"${bootargs_once} ${extra_bootargs}\0" \
-	"install_linux_fw_sd=if load mmc 0 ${loadaddr} install_linux_fw_sd.scr;then " \
-			"source ${loadaddr};" \
-		"fi;\0" \
-	"linux_file=dey-image-qt-x11-" CONFIG_SYS_BOARD ".boot.ubifs\0" \
-	"loadscript=" \
-		"if test -z \"${mtdbootpart}\"; then " \
-			"setenv mtdbootpart " CONFIG_LINUX_PARTITION ";" \
-		"fi;" \
-		"if ubi part ${mtdbootpart}; then " \
-			"if ubifsmount ubi0:${mtdbootpart}; then " \
-				"ubifsload ${loadaddr} ${script};" \
-			"fi;" \
-		"fi;\0" \
-	"mtdbootpart=" CONFIG_LINUX_PARTITION "\0" \
-	"mtdlinuxindex=" CONFIG_ENV_MTD_LINUX_INDEX "\0" \
-	"mtdrecoveryindex=" CONFIG_ENV_MTD_RECOVERY_INDEX "\0" \
-	"mtdrootfsindex=" CONFIG_ENV_MTD_ROOTFS_INDEX "\0" \
-	"mtdupdateindex=" CONFIG_ENV_MTD_UPDATE_INDEX "\0" \
-	"recoverycmd=" \
-		"setenv mtdbootpart " CONFIG_RECOVERY_PARTITION ";" \
-		"boot\0" \
-	"rootfs_file=dey-image-qt-x11-" CONFIG_SYS_BOARD ".ubifs\0" \
-	""	/* end line */
-#else
-#define CONFIG_EXTRA_ENV_SETTINGS \
-	CONFIG_COMMON_ENV \
-	"loadscript=load mmc ${mmcbootdev}:${mmcpart} ${loadaddr} ${script}\0" \
-	"mmcdev="__stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
-	"mmcpart=" __stringify(CONFIG_SYS_MMC_IMG_LOAD_PART) "\0" \
-	"mmcroot=" CONFIG_MMCROOT " rootwait rw\0" \
-	"mmcautodetect=yes\0" \
-	"mmcargs=setenv bootargs console=${console},${baudrate} " \
-		"${mtdparts} " \
-		"root=${mmcroot}\0" \
-	""	/* end line */
-#endif
+		"fdtaddr=0x83000000\0" \
+		"fdtfile=/boot/linux.dtb\0" \
+		"kernelfile=/boot/zImage\0" \
+		"console=" CONFIG_CONSOLE_PORT "\0" \
+		"mmcdev=0\0" \
+		"mmcrootpart=2\0" \
+		"loadfdt=ext4load mmc ${mmcdev}:${mmcrootpart} ${fdtaddr} ${fdtfile}\0" \
+		"loadkernel=ext4load mmc ${mmcdev}:${mmcrootpart} ${loadaddr} ${kernelfile}\0" \
+		"loadall=mmc dev ${mmcdev} && mmc rescan && run loadkernel && run loadfdt\0" \
+		"bootargs=rootwait\0" \
+		"setconsoleargs=setenv bootargs ${bootargs} console=${console},${baudrate}\0" \
+		"setfsargs=setenv bootargs ${bootargs} root=/dev/mmcblk${mmcdev}p${mmcrootpart} rootfstype=ext4 rw\0" \
+		"setbootargs=run setconsoleargs && run setfsargs && echo Set bootargs to ${bootargs}...\0" \
+		"bootkernel=bootz ${loadaddr} - ${fdtaddr}\0"
 
-#define CONFIG_SYS_MMC_ENV_DEV		0   /* USDHC2 */
-#define CONFIG_SYS_MMC_ENV_PART		0	/* user area */
-#define CONFIG_MMCROOT			"/dev/mmcblk1p2"  /* USDHC2 */
+#define CONFIG_BOOTCOMMAND \
+		"echo DEFAULT BOOT !! BOOT DRIVE IS CORRUPTED !!" \
+		" && run loadall"\
+		" && run setbootargs"\
+		" && run bootkernel"
+
+#define CONFIG_BOOTDELAY               3
+
+#define CONFIG_SYS_MMC_ENV_DEV	0
+#define CONFIG_FEC_ENET_DEV	0
 
 /* Carrier board version in OTP bits */
+
 #define CONFIG_HAS_CARRIERBOARD_VERSION
+
 #ifdef CONFIG_HAS_CARRIERBOARD_VERSION
+
 /* The carrier board version is stored in Bank 4 Word 6 (GP1)
  * in bits 3..0 */
 #define CONFIG_CARRIERBOARD_VERSION_BANK	4
@@ -236,6 +126,7 @@
 #define CONFIG_CARRIERBOARD_VERSION_MASK	0xf
 #define CONFIG_CARRIERBOARD_VERSION_OFFSET	0
 #endif /* CONFIG_HAS_CARRIERBOARD_VERSION */
+
 
 /* Carrier board ID in OTP bits */
 #define CONFIG_HAS_CARRIERBOARD_ID
@@ -247,10 +138,8 @@
 #define CONFIG_CARRIERBOARD_ID_MASK	0xff
 #define CONFIG_CARRIERBOARD_ID_OFFSET	4
 #endif /* CONFIG_HAS_CARRIERBOARD_ID */
-
-#define CONFIG_BOOTDELAY               1
-
 /* UBI and UBIFS support */
+
 #define CONFIG_CMD_UBI
 #define CONFIG_RBTREE
 #define CONFIG_CMD_UBIFS
